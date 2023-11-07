6d4b4554cee1f109e6d8beec00d7943b07f39656
sultan435 <sultanmahmud5998@gmail.com>
2023-11-07 22:31:29 +0600

    this project has been included the update route

--- src/components/booksCart/UpdateBook.jsx
+++ src/components/booksCart/UpdateBook.jsx
@@ -1,7 +1,9 @@
-import { useLoaderData } from "react-router-dom";
-import Title from "../ui/Title";
+import { NavLink, useLoaderData } from "react-router-dom";
+// import Title from "../ui/Title";
 import useAxios from "../../hooks/useAxios";
 import { useQuery } from "@tanstack/react-query";
+import { BsArrowRightShort } from "react-icons/bs";
+import bannerImage from '../../assets/images/banner/c-banner.jpg'
 
 const UpdateBook = () => {
 
@@ -41,11 +43,32 @@ const UpdateBook = () => {
 
     return (
         <div>
-            <Title>Update Book</Title>
-          
-            <div className="bg-[#f2f2f2]">
+          <div className="hero h-[350px]" style={{ backgroundImage: `url(${bannerImage})` }}>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-8 text-5xl font-bold text-black ">Update <span className="text-[#e41f05]">Book</span></h1>
+                        <div className="border-2 border-[#e41f05] w-24 rounded-lg mx-auto"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-slate-100 py-5 flex justify-center items-center">
+                <NavLink
+                    to="/"
+                    className="hover:text-[#e41f05] font-medium uppercase"
+                >
+                    Home
+                </NavLink>
+                <BsArrowRightShort className="text-2xl mx-1" />
+                <NavLink
+                    to="/borrowed"
+                    className="hover:text-[#e41f05] font-medium uppercase"
+                >
+                    Borrow Book
+                </NavLink>
+            </div>
+            <div className="">
                 <div className=' py-16 px-4 lg:px-28 max-w-screen-xl mx-auto '>
-                    <h1 className='text-5xl text-[#374151] mb-8 text-center font-semibold'>Add New <span className="text-[#e41f05]">Book</span></h1>
+                    <h1 className='text-5xl text-[#374151] mb-8 text-center font-semibold'>Update <span className="text-[#e41f05]">Book</span></h1>
                     <form onSubmit={handleUpdateBook} className='mt-10'>
                         <div className='lg:flex md:flex gap-6'>
                             <div className='md:w-1/2 lg:w-1/2'>
@@ -53,14 +76,14 @@ const UpdateBook = () => {
                                     <span className="text-lg pl-1">Name</span>
                                 </label>
                                 <label>
-                                    <input type="text" defaultValue={bookName} name='name' className="border rounded-lg py-3 px-4 bg-white my-2 w-full" />
+                                    <input type="text" defaultValue={bookName} name='name' className="border rounded-lg shadow-md py-3 px-4 bg-white my-2 w-full" />
                                 </label>
                             </div>
                             <div className='md:w-1/2 lg:w-1/2'>
                                 <label >
                                     <span className="text-lg pl-1">Category Name</span>
                                 </label>
-                                <select name="bookCategory" className="border rounded-lg py-3 px-4 bg-white my-2 w-full">
+                                <select name="bookCategory" className="border rounded-lg shadow-md py-3 px-4 bg-white my-2 w-full">
                                     
                                     {
                                         categories?.data?.map((items) => <option key={items._id} defaultValue={items.bookCategory}>{bookCategory}</option>)
@@ -74,7 +97,7 @@ const UpdateBook = () => {
                                     <span className=" text-lg pl-1">Author Name</span>
                                 </label>
                                 <label>
-                                    <input type="text" name='authorName' defaultValue={authorName} className="border rounded-lg py-3 px-4 bg-white my-2 w-full" />
+                                    <input type="text" name='authorName' defaultValue={authorName} className="border shadow-md rounded-lg py-3 px-4 bg-white my-2 w-full" />
                                 </label>
                             </div>
                             <div className='md:w-1/2 lg:w-1/2'>
@@ -82,7 +105,7 @@ const UpdateBook = () => {
                                     <span className="text-lg pl-1">Quantity</span>
                                 </label>
                                 <label>
-                                    <input type="text" name='bookQuantity' defaultValue={bookQuantity}  className="border rounded-lg py-3 px-4 bg-white my-2 w-full" />
+                                    <input type="text" name='bookQuantity' defaultValue={bookQuantity}  className="border shadow-md rounded-lg py-3 px-4 bg-white my-2 w-full" />
                                 </label>
                             </div>
                         </div>
@@ -92,7 +115,7 @@ const UpdateBook = () => {
                                     <span className=" text-lg pl-1">Book Image</span>
                                 </label>
                                 <label>
-                                    <input type="text" name='image' defaultValue={image} className="border rounded-lg py-3 px-4 bg-white my-2 w-full" />
+                                    <input type="text" name='image' defaultValue={image} className="border rounded-lg shadow-md py-3 px-4 bg-white my-2 w-full" />
                                 </label>
                             </div>
                             <div className='md:w-1/2 lg:w-1/2'>
@@ -100,7 +123,7 @@ const UpdateBook = () => {
                                     <span className=" text-lg pl-1">Rating</span>
                                 </label>
                                 <label>
-                                    <input type="text"  name='rating' defaultValue={rating} className="border rounded-lg py-3 px-4 bg-white my-2 w-full" />
+                                    <input type="text"  name='rating' defaultValue={rating} className="border rounded-lg shadow-md py-3 px-4 bg-white my-2 w-full" />
                                 </label>
                             </div>
                         </div>
@@ -109,10 +132,10 @@ const UpdateBook = () => {
                                 <label >
                                     <span className="text-lg pl-1">Short Description</span>
                                 </label>
-                                <textarea className="w-full border rounded-lg py-3 px-4 bg-white my-2" defaultValue={bookDescription} name="bookDescription" id="" cols="30" rows="3"></textarea>
+                                <textarea className="w-full border shadow-md rounded-lg py-3 px-4 bg-white my-2" defaultValue={bookDescription} name="bookDescription" id="" cols="30" rows="3"></textarea>
                             </div>
                         </div>
-                        <input type="submit" value="Add Book" className='py-3 mt-4 cursor-pointer rounded-md w-full border bg-[#e41f05]  text-white text-lg font-semibold' />
+                        <input type="submit" value="Update Book" className='py-3 mt-4 cursor-pointer rounded-md shadow-md w-full border bg-[#e41f05]  text-white text-lg font-semibold' />
                     </form>
                 </div>
             </div>
